c0cb00c5fa7945e0f776b06f6f6cb0b9e2937f44
David Gumberg <davidzgumberg@gmail.com>
2023-11-10 17:32:15 -0500

    Add annotorious-osd

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 import { Dresser, Drawer, DrawerStatus, DrawerHandleStatus } from './dresser.js'
 import OpenSeadragon from 'openseadragon'
 
+// Import annotorious and CSS
+import * as Annotorious from '@recogito/annotorious-openseadragon'
+
 const DZ_URL_PREFIX = "https://infernoparadiso.s3.us-east-2.amazonaws.com/"
 // const DZ_URL_PREFIX = "images/"
 
@@ -18,6 +21,8 @@ var viewer = OpenSeadragon({
         dblClickToZoom: true,
     },
 });
+
+const anno = Annotorious.default(viewer, {});
 
 const elDrawerBooks = document.querySelector('[data-drawer="books"]')
 const elDrawerInferno = document.querySelector ('[data-drawer="inferno"]')
